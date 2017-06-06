af5d14618bd6ccb365994f46a66b1244938191c0
jchristopherson <jchristopherson@hotmail.com>
2017-06-06 10:52:32 -0500

    Updating C bindings

--- src/linalg_c_binding.f90
+++ src/linalg_c_binding.f90
@@ -8,6 +8,7 @@ module linalg_c_binding
     use, intrinsic :: iso_c_binding
     use linalg_constants
     use linalg_core
+    use ferror, only : errors
 contains
 ! ******************************************************************************
 ! LINALG_CORE ROUTINES
@@ -33,7 +34,7 @@ contains
     !! @par Notes
     !! This routine is based upon the BLAS routine DGER.
     subroutine rank1_update_c(m, n, alpha, x, y, a) &
-            bind(c, name = "rank1_update")
+            bind(C, name = "rank1_update")
         ! Arguments
         real(dp), intent(in), value :: alpha
         integer(i32), intent(in), value :: m, n
@@ -58,7 +59,7 @@ contains
     !! @param[in] x The matrix on which to operate.
     !!
     !! @return The trace of @p x.
-    pure function trace_c(m, n, x) result(y) bind(c, name = "trace")
+    pure function trace_c(m, n, x) result(y) bind(C, name = "trace")
         ! Arguments
         integer(i32), intent(in), value :: m, n
         real(dp), intent(in) :: x(m,n)
@@ -68,10 +69,94 @@ contains
         y = trace(x)
     end function
 
+! ------------------------------------------------------------------------------
+    !> @brief Computes the rank of a matrix.
+    !!
+    !! @param[in] m The number of rows in the matrix.
+    !! @param[in] n The number of columns in the matrix.
+    !! @param[in,out] a On input, the M-by-N matrix of interest.  On output, the
+    !!  contents of the matrix are overwritten.
+    !! @param[in] err A pointer to the C error handler object.  If no error
+    !!  handling is desired, simply pass NULL, and errors will be dealt with
+    !!  by the default internal error handler.  Possible errors that may be
+    !!  encountered are as follows.
+    !!  - LA_ARRAY_SIZE_ERROR: Occurs if any of the input arrays are not sized
+    !!      appropriately.
+    !!  - LA_OUT_OF_MEMORY_ERROR: Occurs if local memory must be allocated, and
+    !!      there is insufficient memory available.
+    !!  - LA_CONVERGENCE_ERROR: Occurs as a warning if the QR iteration process
+    !!      could not converge to a zero value.
+    !!
+    !! @par See Also
+    !! - [Wolfram MathWorld](http://mathworld.wolfram.com/MatrixRank.html)
+    function mtx_rank_c(m, n, a, err) result(rnk) bind(C, name = "mtx_rank")
+        ! Arguments
+        integer(i32), intent(in), value :: m, n
+        real(dp), intent(inout) :: a(m,n)
+        type(c_ptr), intent(in), value :: err
+        integer(i32) :: rnk
+
+        ! Local Variables
+        type(errors), pointer :: eptr
+        logical :: useError
+
+        ! Set up error handling
+        if (c_associated(err)) then
+            call c_f_pointer(err, eptr)
+            useError = .true.
+        else
+            useError = .false.
+        end if
+
+        ! Process
+        if (useError) then
+            rnk = mtx_rank(a, err = eptr)
+        else
+            rnk = mtx_rank(a)
+        end if
+    end function
 
 ! ------------------------------------------------------------------------------
+    !> @brief Computes the determinant of a square matrix.
+    !!
+    !! @param[in] n The dimension of the matrix.
+    !! @param[in,out] a On input, the N-by-N matrix on which to operate.  On
+    !! output the contents are overwritten by the LU factorization of the
+    !! original matrix.
+    !! @param[in] err A pointer to the C error handler object.  If no error
+    !!  handling is desired, simply pass NULL, and errors will be dealt with
+    !!  by the default internal error handler.  Possible errors that may be
+    !!  encountered are as follows.
+    !!  - LA_ARRAY_SIZE_ERROR: Occurs if any of the input arrays are not sized
+    !!      appropriately.
+    !!  - LA_OUT_OF_MEMORY_ERROR: Occurs if local memory must be allocated, and
+    !!      there is insufficient memory available.
+    function det_c(n, a, err) result(x) bind(C, name = "det")
+        ! Arguments
+        integer(i32), intent(in), value :: n
+        real(dp), intent(inout) :: a(n,n)
+        type(c_ptr), intent(in), value :: err
+        real(dp) :: x
 
-! ------------------------------------------------------------------------------
+        ! Local Variables
+        type(errors), pointer :: eptr
+        logical :: useError
+
+        ! Set up error handling
+        if (c_associated(err)) then
+            call c_f_pointer(err, eptr)
+            useError = .true.
+        else
+            useError = .false.
+        end if
+
+        ! Process
+        if (useError) then
+            x = det(a, err = eptr)
+        else
+            x = det(a)
+        end if
+    end function
 
 ! ------------------------------------------------------------------------------
 

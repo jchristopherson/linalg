43a86d1bc6c35d9a34943d3b8a87901b85a1dcb4
jchristopherson <jchristopherson@hotmail.com>
2017-07-24 05:39:16 -0500

    Add an LU factorization example

---
+++ examples/linalg_lu_example.f90
@@ -0,0 +1,36 @@
+! linalg_lu_example.f90
+
+! Example Source: 
+! https://www.mathworks.com/help/matlab/ref/lu.html?s_tid=srchtitle
+program example
+    use linalg_constants, only : dp, i32
+    use linalg_factor, only : lu_factor
+    use linalg_solve, only : lu_solve
+    implicit none
+
+    ! Local Variables
+    real(dp) :: a(3,3), b(3)
+    integer(i32) :: pvt(3)
+
+    ! Build the 3-by-3 matrix A.
+    !     | 1   2   3 |
+    ! A = | 4   5   6 |
+    !     | 7   8   0 |
+    a = reshape( &
+        [1.0d0, 4.0d0, 7.0d0, 2.0d0, 5.0d0, 8.0d0, 3.0d0, 6.0d0, 0.0d0], &
+        [3, 3])
+    
+    ! Build the right-hand-side vector B.
+    !     | -1 |
+    ! b = | -2 |
+    !     | -3 |
+    b = [-1.0d0, -2.0d0, -3.0d0]
+
+    ! Compute the LU factorization
+    call lu_factor(a, pvt)
+
+    ! Compute the solution.  The results overwrite b.
+    call solve_lu(a, pvt, b)
+
+    ! Display the results.
+end program

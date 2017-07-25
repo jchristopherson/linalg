2d5b605aab87532c98938b0fbf6d91cd6e8edcde
jchristopherson <jchristopherson@hotmail.com>
2017-07-25 16:00:54 -0500

    Clean up LU example (C version)

--- examples/linalg_lu_c_example.c
+++ examples/linalg_lu_c_example.c
@@ -5,21 +5,18 @@
 #include "ferror.h"
 
 int main() {
-    // Local Variables
-    double a[9], b[3];
-    int pvt[3];
-
-    // Build the 3-by-3 matrix A - stored in column-major format.
+    // Define local variables and populate the matrices (column-major format):
+    //
     //     | 1   2   3 |
     // A = | 4   5   6 |
     //     | 7   8   0 |
-    a = [1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 0.0];
-
-    // Build the right-hand-side vector B.
+    //
     //     | -1 |
     // b = | -2 |
     //     | -3 |
-    b = [-1.0d0, -2.0d0, -3.0d0]
+    double a[9] = {1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 0.0},
+        b[3] = {-1.0, -2.0, -3.0};
+    int pvt[3];
 
     // The solution is:
     //     |  1/3 |

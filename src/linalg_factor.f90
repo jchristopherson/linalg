0e71b44cc77cc3e5f56ba53f78b07e7917a3c4a7
Jason Christopherson <jchristopherson@hotmail.com>
2018-04-11 09:49:55 -0500

    Add examples

--- src/linalg_factor.f90
+++ src/linalg_factor.f90
@@ -1869,6 +1869,71 @@ contains
     !!      positive definite.
     !!  - LA_SINGULAR_MATRIX_ERROR: Occurs if @p r is singular.
     !!
+    !! @par Usage
+    !! The following example illustrates the use of the rank 1 Cholesky 
+    !! downdate, and compares the results to factoring the original rank 1 
+    !! downdated matrix.
+    !! @code{.f90}
+    !! program example
+    !!     use iso_fortran_env, only : real64, int32
+    !!     use linalg_factor, only : cholesky_factor, cholesky_rank1_downdate
+    !!     use linalg_core, only : rank1_update
+    !!     implicit none
+    !!
+    !!     ! Variables
+    !!     real(real64) :: a(3,3), u(3), ad(3,3)
+    !!     integer(int32) :: i
+    !!
+    !!     ! Build the 3-by-3 matrix A.
+    !!     !     |  4.25   11.25   -15 |
+    !!     ! A = | 11.25   39.25   -46 |
+    !!     !     |  -15     -46    102 |
+    !!     a = reshape([4.25d0, 11.25d0, -15.0d0, 11.25d0, 39.25d0, -46.0d0, &
+    !!         -15.0d0, -46.0d0, 102.0d0], [3, 3])
+    !!
+    !!     ! The downdate vector
+    !!     !     |  0.5 |
+    !!     ! u = | -1.5 |
+    !!     !     |   2  |
+    !!     u = [0.5d0, -1.5d0, 2.0d0]
+    !!
+    !!     ! Compute the rank 1 downdate of A
+    !!     ad = a
+    !!     call rank1_update(-1.0d0, u, u, ad)
+    !!
+    !!     ! Compute the Cholesky factorization of the original matrix
+    !!     call cholesky_factor(a)
+    !!
+    !!     ! Apply the rank 1 downdate to the factored matrix
+    !!     call cholesky_rank1_downdate(a, u)
+    !!
+    !!     ! Compute the Cholesky factorization of the downdate to the original matrix
+    !!     call cholesky_factor(ad)
+    !!
+    !!     ! Display the matrices
+    !!     print '(A)', "Downdating the Factored Form:"
+    !!     do i = 1, size(a, 1)
+    !!         print *, a(i,:)
+    !!     end do
+    !!
+    !!     print '(A)', "Downdating A Directly:"
+    !!     do i = 1, size(ad, 1)
+    !!         print *, ad(i,:)
+    !!     end do
+    !! end program
+    !! @endcode
+    !! The above program produces the following output.
+    !! @code{.txt}
+    !! Downdating the Factored Form:
+    !!  2.0000000000000000        6.0000000000000000       -8.0000000000000000
+    !!  0.0000000000000000        1.0000000000000000        4.9999999999999973
+    !!  0.0000000000000000        0.0000000000000000        3.0000000000000049
+    !! Downdating A Directly:
+    !!  2.0000000000000000        6.0000000000000000       -8.0000000000000000
+    !!  0.0000000000000000        1.0000000000000000        5.0000000000000000
+    !!  0.0000000000000000        0.0000000000000000        3.0000000000000000
+    !! @endcode
+    !!
     !! @par Notes
     !! This routine utilizes the QRUPDATE routine DCH1DN.
     !!

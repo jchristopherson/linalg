2d1b05e4ff3ad934cfd7a672d5089b17430c459c
jchristopherson <jchristopherson@hotmail.com>
2022-12-20 06:15:45 -0600

    Update documentation

--- doc/html/search/classes_4.js
+++ doc/html/search/classes_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['lu_5ffactor_0',['lu_factor',['../interfacelinalg_1_1lu__factor.html',1,'linalg']]]
+  ['lq_5ffactor_0',['lq_factor',['../interfacelinalg_1_1lq__factor.html',1,'linalg']]],
+  ['lu_5ffactor_1',['lu_factor',['../interfacelinalg_1_1lu__factor.html',1,'linalg']]]
 ];
--- doc/html/search/classes_5.js
+++ doc/html/search/classes_5.js
@@ -4,6 +4,7 @@ var searchData=
   ['mtx_5fmult_1',['mtx_mult',['../interfacelinalg_1_1mtx__mult.html',1,'linalg']]],
   ['mtx_5fpinverse_2',['mtx_pinverse',['../interfacelinalg_1_1mtx__pinverse.html',1,'linalg']]],
   ['mtx_5frank_3',['mtx_rank',['../interfacelinalg_1_1mtx__rank.html',1,'linalg']]],
-  ['mult_5fqr_4',['mult_qr',['../interfacelinalg_1_1mult__qr.html',1,'linalg']]],
-  ['mult_5frz_5',['mult_rz',['../interfacelinalg_1_1mult__rz.html',1,'linalg']]]
+  ['mult_5flq_4',['mult_lq',['../interfacelinalg_1_1mult__lq.html',1,'linalg']]],
+  ['mult_5fqr_5',['mult_qr',['../interfacelinalg_1_1mult__qr.html',1,'linalg']]],
+  ['mult_5frz_6',['mult_rz',['../interfacelinalg_1_1mult__rz.html',1,'linalg']]]
 ];

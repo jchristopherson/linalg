76e7aad0903b332569143411dec9f94b86b566f1
jchristopherson <jchristopherson@hotmail.com>
2022-12-22 14:01:20 -0600

    Fix find_package related bug in build scripts

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -60,7 +60,7 @@ endfunction()
 # Links the supplied library
 function(link_library targ lib include_dir)
     target_link_libraries(${targ} ${lib})
-    target_include_directories(${targ} PUBLIC ${include_dir})
+    target_include_directories(${targ} PUBLIC $<BUILD_INTERFACE:${include_dir}>)
 endfunction()
 
 # ------------------------------------------------------------------------------

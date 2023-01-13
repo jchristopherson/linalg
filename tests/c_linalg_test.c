6135d3ff8eda1e5f202dbba1b1e23d2c02e8d619
jchristopherson <jchristopherson@hotmail.com>
2023-01-13 11:26:50 -0600

    Troubleshooting

--- tests/c_linalg_test.c
+++ tests/c_linalg_test.c
@@ -8,35 +8,35 @@ int main()
     int flag = 0;
 
     // Tests
-    check = test_diagonal_mtx_mult();
-    if (!check) flag = 1;
-    check = test_cmplx_diagonal_mtx_mult();
-    if (!check) flag = -1;
+    // check = test_diagonal_mtx_mult();
+    // if (!check) flag = 1;
+    // check = test_cmplx_diagonal_mtx_mult();
+    // if (!check) flag = -1;
 
-    check = test_rank1_update();
-    if (!check) flag = 2;
-    check = test_cmplx_rank1_update();
-    if (!check) flag = -2;
+    // check = test_rank1_update();
+    // if (!check) flag = 2;
+    // check = test_cmplx_rank1_update();
+    // if (!check) flag = -2;
 
-    check = test_trace();
-    if (!check) flag = 3;
-    check = test_cmplx_trace();
-    if (!check) flag = -3;
+    // check = test_trace();
+    // if (!check) flag = 3;
+    // check = test_cmplx_trace();
+    // if (!check) flag = -3;
 
-    check = test_triangular_matrix_multiply();
-    if (!check) flag = 4;
-    check = test_cmplx_triangular_matrix_multiply();
-    if (!check) flag = -4;
+    // check = test_triangular_matrix_multiply();
+    // if (!check) flag = 4;
+    // check = test_cmplx_triangular_matrix_multiply();
+    // if (!check) flag = -4;
 
-    check = test_lu();
-    if (!check) flag = 5;
-    check = test_cmplx_lu();
-    if (!check) flag = -5;
+    // check = test_lu();
+    // if (!check) flag = 5;
+    // check = test_cmplx_lu();
+    // if (!check) flag = -5;
 
-    check = test_qr();
-    if (!check) flag = 6;
-    check = test_cmplx_qr();
-    if (!check) flag = -6;
+    // check = test_qr();
+    // if (!check) flag = 6;
+    // check = test_cmplx_qr();
+    // if (!check) flag = -6;
 
     check = test_qr_pivot();
     if (!check) flag = 7;

ddc2451c7ac8951b4f2e614ba095495072bfc086
jchristopherson <jchristopherson@hotmail.com>
2023-01-18 12:26:35 -0600

    Update tests

--- tests/c_linalg_test.c
+++ tests/c_linalg_test.c
@@ -55,6 +55,8 @@ int main()
 
     check = test_cholesky_rank1_update();
     if (!check) flag = 10;
+    check = test_cmplx_cholesky_rank1_update();
+    if (!check) flag = -10;
     
     check = test_svd();
     if (!check) flag = 11;
@@ -80,13 +82,8 @@ int main()
     check = test_lq();
     if (!check) flag = 16;
 
-/* STUFF THAT NEEDS MORE WORK BEFORE RELEASING INTO THE WILD */
-
-    // check = test_cmplx_cholesky_rank1_update();
-    // if (!check) flag = -10;
-
-    // check = test_cmplx_lq();
-    // if (!check) flag = 17;
+    check = test_cmplx_lq();
+    if (!check) flag = 17;
 
     // End
     return flag;

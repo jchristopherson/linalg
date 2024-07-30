33441c2839944f95d47b9084455fe34f1716cf33
Jason Christopherson <jchristopherson@hotmail.com>
2024-07-30 06:22:00 -0500

    Update documentation

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -3,8 +3,11 @@ var searchData=
   ['cholesky_5ffactor_0',['cholesky_factor',['../interfacelinalg_1_1cholesky__factor.html',1,'linalg']]],
   ['cholesky_5frank1_5fdowndate_1',['cholesky_rank1_downdate',['../interfacelinalg_1_1cholesky__rank1__downdate.html',1,'linalg']]],
   ['cholesky_5frank1_5fupdate_2',['cholesky_rank1_update',['../interfacelinalg_1_1cholesky__rank1__update.html',1,'linalg']]],
-  ['column_5findices_3',['column_indices',['../structlinalg_1_1csr__matrix.html#acb77b1101f32cefe10e1d15fdd87d307',1,'linalg::csr_matrix']]],
-  ['csr_5fmatrix_4',['csr_matrix',['../structlinalg_1_1csr__matrix.html',1,'linalg']]],
-  ['csrcsc2_5',['csrcsc2',['../interfacesparskit_1_1csrcsc2.html',1,'sparskit']]],
-  ['csrmsr_6',['csrmsr',['../interfacesparskit_1_1csrmsr.html',1,'sparskit']]]
+  ['clncsr_3',['clncsr',['../interfacesparskit_1_1clncsr.html',1,'sparskit']]],
+  ['column_5findices_4',['column_indices',['../structlinalg_1_1csr__matrix.html#acb77b1101f32cefe10e1d15fdd87d307',1,'linalg::csr_matrix']]],
+  ['coocsr_5',['coocsr',['../interfacesparskit_1_1coocsr.html',1,'sparskit']]],
+  ['csort_6',['csort',['../interfacesparskit_1_1csort.html',1,'sparskit']]],
+  ['csr_5fmatrix_7',['csr_matrix',['../structlinalg_1_1csr__matrix.html',1,'linalg']]],
+  ['csrcsc2_8',['csrcsc2',['../interfacesparskit_1_1csrcsc2.html',1,'sparskit']]],
+  ['csrmsr_9',['csrmsr',['../interfacesparskit_1_1csrmsr.html',1,'sparskit']]]
 ];

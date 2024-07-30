1ead2be7edd03d33b92e12b37e5e40c272ad7cb2
jchristopherson <jchristopherson@hotmail.com>
2024-07-30 12:30:34 +0000

    Deploying to gh-pages from @ jchristopherson/linalg@3f34fad18a89d864186488b5315c1da097b57946 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -3,7 +3,10 @@ var searchData=
   ['cholesky_5ffactor_0',['cholesky_factor',['../interfacelinalg_1_1cholesky__factor.html',1,'linalg']]],
   ['cholesky_5frank1_5fdowndate_1',['cholesky_rank1_downdate',['../interfacelinalg_1_1cholesky__rank1__downdate.html',1,'linalg']]],
   ['cholesky_5frank1_5fupdate_2',['cholesky_rank1_update',['../interfacelinalg_1_1cholesky__rank1__update.html',1,'linalg']]],
-  ['csr_5fmatrix_3',['csr_matrix',['../structlinalg_1_1csr__matrix.html',1,'linalg']]],
-  ['csrcsc2_4',['csrcsc2',['../interfacesparskit_1_1csrcsc2.html',1,'sparskit']]],
-  ['csrmsr_5',['csrmsr',['../interfacesparskit_1_1csrmsr.html',1,'sparskit']]]
+  ['clncsr_3',['clncsr',['../interfacesparskit_1_1clncsr.html',1,'sparskit']]],
+  ['coocsr_4',['coocsr',['../interfacesparskit_1_1coocsr.html',1,'sparskit']]],
+  ['csort_5',['csort',['../interfacesparskit_1_1csort.html',1,'sparskit']]],
+  ['csr_5fmatrix_6',['csr_matrix',['../structlinalg_1_1csr__matrix.html',1,'linalg']]],
+  ['csrcsc2_7',['csrcsc2',['../interfacesparskit_1_1csrcsc2.html',1,'sparskit']]],
+  ['csrmsr_8',['csrmsr',['../interfacesparskit_1_1csrmsr.html',1,'sparskit']]]
 ];

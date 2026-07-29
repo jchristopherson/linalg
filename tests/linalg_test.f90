1ea62a52ccafe5375cdd1896c45dcc3ff59ea37e
jchristopherson <jchristopherson@hotmail.com>
2026-07-29 15:14:29 -0500

    Troubleshooting

--- tests/linalg_test.f90
+++ tests/linalg_test.f90
@@ -230,11 +230,11 @@ program main
     rst = test_eigen_symm()
     if (.not.rst) flag = 66
     
-    ! rst = test_eigen_asymm()
-    ! if (.not.rst) flag = 67
+    rst = test_eigen_asymm()
+    if (.not.rst) flag = 67
 
-    ! rst = test_eigen_asymm_cmplx()
-    ! if (.not.rst) flag = 68
+    rst = test_eigen_asymm_cmplx()
+    if (.not.rst) flag = 68
     
     rst = test_eigen_gen()
     if (.not.rst) flag = 69

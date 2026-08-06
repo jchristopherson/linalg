a23153e2b2f1fb93418322823e24342bf4532cb5
jchristopherson <jchristopherson@hotmail.com>
2026-08-06 10:26:34 -0500

    Remove FERROR dependency

--- configure/template.cmake
+++ configure/template.cmake
@@ -1,8 +1,5 @@
 @PACKAGE_INIT@
 
-include(CMakeFindDependencyMacro)
-find_dependency(ferror QUIET)
-
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 endif()
